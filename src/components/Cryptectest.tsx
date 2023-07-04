fbf64cc66ac4d8a5eb9e58c56d540ebdf5aa7a29
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-07-05 01:40:10 +0800

    sec

--- src/components/Cryptectest.tsx
+++ src/components/Cryptectest.tsx
@@ -23,13 +23,12 @@ export default class Cryptectest extends Component<Props, State> {
                     src="/images/ship-2.webp"
                     width="750" height="750"
                     />
-                <div className='to-border'>
-            <div data-aos="fade-right"
-                    data-aos-duration="1500" data-aos-anchor-placement="bottom-bottom" className='color-effect section-title'> CRYPTECHTEST GAME </div>
+                <div data-aos="fade-right"
+                    data-aos-duration="1500" data-aos-anchor-placement="bottom-bottom" className='to-border'>
+            <div className='color-effect section-title'> CRYPTECHTEST GAME </div>
                         <div data-aos="fade-up"
                     data-aos-duration="1500" className='section-desc'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-                        <a data-aos="fade-up"
-                    data-aos-duration="1500" className="home-button" href="https://cryptechtest.xyz/" target='_blank'><div><button type="button" className="home-button-txt">Join Us!</button></div></a>
+                        <a className="home-button" href="https://cryptechtest.xyz/" target='_blank'><div><button type="button" className="home-button-txt">Join Us!</button></div></a>
                 </div>
                 </div>
                                 </div>
