aa85a94491b9071139919a230a3936e4b7f078d2
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-07-05 01:47:19 +0800

    supp

--- src/components/Cryptectest.tsx
+++ src/components/Cryptectest.tsx
@@ -27,7 +27,7 @@ export default class Cryptectest extends Component<Props, State> {
                     data-aos-duration="1500" data-aos-anchor-placement="bottom-bottom" className='to-border'>
             <div className='color-effect section-title'> CRYPTECHTEST GAME </div>
                         <div data-aos="fade-up"
-                    data-aos-duration="1500" className='section-desc'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                    data-aos-duration="1500" data-aos-anchor-placement="bottom-bottom" className='section-desc'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
                         <a className="home-button" href="https://cryptechtest.xyz/" target='_blank'><div><button type="button" className="home-button-txt">Join Us!</button></div></a>
                 </div>
                 </div>
--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -27,7 +27,8 @@ export default class Support extends Component<Props, State> {
                     />
                 <div data-aos="fade-right"
               data-aos-duration="1500"
-              data-aos-anchor-placement="bottom-bottom" className='section-desc supp-text-border'> Please contact <span className='color-effect-nontitle highlighted'> SQUIDICUZ </span> if it is urgent, or contact another admin to attempt to resolve your issue. <span className='color-effect-nontitle highlighted'> Thank you. </span> </div>
+              data-aos-anchor-placement="bottom-bottom" className='section-desc supp-text-border'> Please contact <span className='color-effect-nontitle highlighted'> SQUIDICUZ </span> if it is urgent, or contact another admin to attempt to resolve your issue. <span className='color-effect-nontitle highlighted'> Thank you. </span>
+            <a className="home-button" href="https://cryptechtest.xyz/" target='_blank'><div><button type="button" className="home-button-txt">Contact</button></div></a></div>
                 </div>
                 </div>
       </section>
