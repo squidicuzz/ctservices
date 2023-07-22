21e088680adab5931b41b6abef19c85da45a3be7
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-07-22 13:22:08 +0800

    hi

--- src/layout/sections/Contact.tsx
+++ src/layout/sections/Contact.tsx
@@ -24,32 +24,38 @@ return (
          </div>
          <div className='lest'>
             <ul>
-    <li>
+    <li data-aos="fade-down"
+            data-aos-duration="500" data-aos-anchor-placement="bottom-bottom">
         <div className="icon"><LayersOutlinedIcon className='ico'/></div>
         <div className="title">Fullstack Web Development</div>
         {/* <div className="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, impedit?</div> */}
     </li>
-    <li>
-        <div className="icon"><DesignServicesOutlinedIcon className='ico'/></div>
-        <div className="title">Web Design</div>
-        {/* <div className="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div> */}
-    </li>
-    <li>
+    <li data-aos="fade-down"
+            data-aos-duration="1500" data-aos-anchor-placement="bottom-bottom">
         <div className="icon"><CurrencyBitcoinOutlinedIcon className='ico'/></div>
         <div className="title">Web3 and DAPP Development</div>
         {/* <div className="descr">Lorem ipsum dolor sit.</div> */}
     </li>
-    <li>
+    <li data-aos="fade-down"
+            data-aos-duration="2000" data-aos-anchor-placement="bottom-bottom">
         <div className="icon"><CodeOutlinedIcon className='ico'/></div>
         <div className="title">Solidity smart contract Development</div>
         {/* <div className="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor laboriosam odio alias.</div> */}
+               </li>
+                   <li data-aos="fade-down"
+            data-aos-duration="1000" data-aos-anchor-placement="bottom-bottom">
+        <div className="icon"><DesignServicesOutlinedIcon className='ico'/></div>
+        <div className="title">Web Design</div>
+        {/* <div className="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div> */}
     </li>
-    <li>
+    <li data-aos="fade-down"
+            data-aos-duration="2500" data-aos-anchor-placement="bottom-bottom">
         <div className="icon"><DrawOutlinedIcon className='ico'/></div>
         <div className="title">Graphic Design</div>
         {/* <div className="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div> */}
                </li>
-                   <li>
+                   <li data-aos="fade-down"
+            data-aos-duration="3000" data-aos-anchor-placement="bottom-bottom">
         <div className="icon"><SupportAgentOutlinedIcon className='ico'/></div>
         <div className="title">Technical Consultation</div>
         {/* <div className="descr">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div> */}
