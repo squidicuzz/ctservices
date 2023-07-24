92fd31388b65d69d105b19fcba27fb84df1f9f67
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-07-24 21:43:56 +0800

    16

--- public/scripts/parallax.js
+++ public/scripts/parallax.js
@@ -4,7 +4,7 @@ let primary_front_bg = document.getElementById("primary-front-bg");
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
   secondary_front_bg.style.top = value * 0.1 + "px";
-  secondary_front_bg.style.left = value * -0.025 + "px";
+  // secondary_front_bg.style.left = value * -0.025 + "px";
   primary_front_bg.style.left = value * -0.5 + "px";
 });
 
