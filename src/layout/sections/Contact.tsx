5138bfd95d8f7f057ae2810310a6f890809b5821
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-08-06 03:02:45 +0800

    h1

--- src/layout/sections/Contact.tsx
+++ src/layout/sections/Contact.tsx
@@ -33,9 +33,9 @@ return (
    <div className="to-width to-height to-center to-flex-column">
       <div className='to-left'>
          
-         {/* <div className='text-motion section-title-logo to-quaternary'> ❖ </div>
+         <div className='text-motion section-title-logo to-quaternary'> ❖ </div>
         <div className='title-support'>
-                <div className='text-motion section-title to-quaternary'> CRYPTECH SERVICES </div> </div> */}
+                <div className='text-motion section-title to-quaternary'> CRYPTECH SERVICES </div> </div>
          {/* <div className='section-desc-1'> For <span className='we-logo-fourth to-bold' >DEVELOPMENT</span> services we offer: </div> */}
             </div>
                             {/* <ImageSlider images={images} /> */}
