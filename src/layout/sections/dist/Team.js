27b528f4282ee03af965741905438df35d9f74b1
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-08-08 08:04:25 +0800

    Update Team.js

--- src/layout/sections/dist/Team.js
+++ src/layout/sections/dist/Team.js
@@ -126,7 +126,8 @@ var Team = /** @class */ (function (_super) {
                                             react_1["default"].createElement(AlternateEmailOutlined_1["default"], null),
                                             " ",
                                             "Email")))))))),
-            react_1["default"].createElement(image_1["default"], { className: "team-land-pic", alt: 'front-logo', src: "/images/team3.webp", width: "3620", height: "1670" })));
+            react_1["default"].createElement(image_1["default"], { className: "team-land-pic", alt: 'front-logo', src: "/images/team3.webp", width: "3620", height: "1670" }),
+            react_1["default"].createElement(image_1["default"], { className: "sales-1-land-pic", alt: 'front-logo', src: "/images/to-center-up.webp", width: "3620", height: "1377" })));
     };
     return Team;
 }(react_1.Component));
