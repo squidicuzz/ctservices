0a90221856d0bcd8cfc2fc380145d9c69e351593
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2023-08-24 08:52:57 +0800

    hi

--- src/layout/components/scripts.js
+++ src/layout/components/scripts.js
@@ -4,6 +4,12 @@ export default function AllScripts() {
   return (
     <>
       <Script
+        rel="preload"
+        src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"
+        as="script"
+        type="text/javascript"
+      />
+      <Script
         src="/scripts/particlescdn.js"
         as="script"
         type="text/javascript"
